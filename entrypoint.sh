8159f129bfad1314339c45b95d5f8fa12481754c
Jared Hall Garst <jhg@fastmail.com>
2020-12-14 11:30:55 -0500

    postgres: database mirrors local environment

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,13 @@ PGDATA=${PGDATA:-/home/jovyan/srv/pgsql}
 
 if [ ! -d "$PGDATA" ]
 then
-    /usr/lib/postgresql/12/bin/initdb -D "$PGDATA" --auth-host=md5 --encoding=UTF8
+    /usr/lib/postgresql/12/bin/initdb -D "$PGDATA" --encoding=UTF8
 fi
 
 /usr/lib/postgresql/12/bin/pg_ctl -D "$PGDATA" status \
 || /usr/lib/postgresql/12/bin/pg_ctl -D "$PGDATA" -l "$PGDATA/pg.log" start
 
-psql postgres -c "CREATE USER test PASSWORD 'testpass'"
-createdb -O test test
+psql postgres -c "CREATE USER jared"
+createdb -O jared jared
 
 exec "$@"
